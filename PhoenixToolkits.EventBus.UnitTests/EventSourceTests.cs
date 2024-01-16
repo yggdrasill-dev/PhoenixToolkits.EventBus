55afdd10081187965caede42f3f414634de34fce
Phoenix <phoenix.chen@rayprosoft.com>
2024-01-16 16:20:27 +0800

    BroadcastBlock 似乎會接收不到資料，改用 Rx.Net 的 Subject

--- PhoenixToolkits.EventBus.UnitTests/EventSourceTests.cs
+++ PhoenixToolkits.EventBus.UnitTests/EventSourceTests.cs
@@ -1,23 +1,25 @@
 ﻿using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
 using Valhalla.Messages;
 
 namespace PhoenixToolkits.Messages;
 
 public class EventSourceTests
 {
-	[Fact]
-	public async Task 送入的訊息可以透過Observable取得()
-	{
-		// Arrange
-		var sut = new EventSource<Guid>();
+    [Fact(Timeout = 1000)]
+    public async Task 送入的訊息可以透過Observable取得()
+    {
+        // Arrange
+        var sut = new EventSource<Guid>();
 
-		var message = Guid.NewGuid();
+        var message = Guid.NewGuid();
 
-		// Act
-		await sut.PublishAsync(message);
-		var actual = await sut.Observable.FirstAsync();
+        // Act
+        var task = sut.Observable.FirstAsync().ToTask();
+        _ = await sut.PublishAsync(message);
+        var actual = await task;
 
-		// Assert
-		Assert.Equal(message, actual);
-	}
+        // Assert
+        Assert.Equal(message, actual);
+    }
 }
--- PhoenixToolkits.EventBus/EventSource.cs
+++ PhoenixToolkits.EventBus/EventSource.cs
@@ -1,13 +1,19 @@
-﻿using System.Threading.Tasks.Dataflow;
+﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks.Dataflow;
 
 namespace Valhalla.Messages;
 
 internal class EventSource<TEventData> : IEventSource<TEventData>
 {
-	private readonly BroadcastBlock<TEventData> m_Broadcast = new(null);
+	private readonly Subject<TEventData> m_Subject = new();
 
-	public IObservable<TEventData> Observable => m_Broadcast.AsObservable();
+	public IObservable<TEventData> Observable => m_Subject.AsObservable();
 
 	public Task<bool> PublishAsync(TEventData eventData, CancellationToken cancellationToken = default)
-		=> m_Broadcast.SendAsync(eventData, cancellationToken);
+	{
+		m_Subject.OnNext(eventData);
+
+		return Task.FromResult(true);
+	}
 }
